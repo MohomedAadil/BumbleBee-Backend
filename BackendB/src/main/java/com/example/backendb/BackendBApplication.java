bfbb0e6d4b09e928e1e34c7f0d16b16ca6488e3b
MohomedAadil <aadil.mohomed786@gmail.com>
2023-03-28 20:17:33 +0530

    Did some file changes and completed the Product table.

---
+++ BackendB/src/main/java/com/example/backendb/BackendBApplication.java
@@ -0,0 +1,14 @@
+package com.example.backendb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BackendBApplication.class, args);
+    }
+
+}
